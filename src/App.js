0d7205a72b06419f4545f817af5d6c1fce255133
MasonPartridge <masonepartridge@gmail.com>
2023-05-04 18:44:57 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, createElement } from 'react';
 
 function App() {
+
+  const [testElements, setTestElements] = useState([]);
+
+  function addTestElement() {
+    setTestElements(testElements.concat(
+      {
+        id: testElements.length
+      }
+    ));
+  }
+
+  function removeTestElement(indexToRemove) {
+    setTestElements( () => 
+      testElements.filter((element, index) => index != indexToRemove).map((task, index) => {
+        return {id: index};
+      })
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Task App</h1>
+      <button 
+        onClick={
+          (e) => {
+            addTestElement();
+          }
+        }>
+        ADD TASK!
+      </button>
+      <div>
+        <ul>
+          {testElements.map((task) => 
+          <li key={task.id}>
+            <input placeholder={'New Task #' + task.id}></input>
+            <input placeholder='Description'></input>
+            <button 
+              onClick={() => removeTestElement(task.id)}
+            >Done</button>
+          </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
+
 }
+
 
 export default App;
